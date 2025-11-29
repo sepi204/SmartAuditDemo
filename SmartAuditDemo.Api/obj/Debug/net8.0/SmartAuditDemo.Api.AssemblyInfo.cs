ab98c60cef3f6c231b739f4d7e856b440d3b169c
sepehr <sepehr.rasoli83@gmail.com>
2025-11-29 15:35:53 +0330

    ...

--- SmartAuditDemo.Api/obj/Debug/net8.0/SmartAuditDemo.Api.AssemblyInfo.cs
+++ SmartAuditDemo.Api/obj/Debug/net8.0/SmartAuditDemo.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartAuditDemo.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60cff3ef4138222704d2de4fada354916338076b")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartAuditDemo.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartAuditDemo.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
